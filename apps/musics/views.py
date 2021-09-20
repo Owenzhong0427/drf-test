25d37385c4679016ab88b4085e67f28a09fe4134
owenzhong0427 <owenzhong@ponddy-edu.com>
2021-09-20 20:38:44 +0800

    Add for apps/musics can works

--- apps/musics/views.py
+++ apps/musics/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Music
+from .models import Music 
 from rest_framework import viewsets, status
 from .serializers import MusicSerializer
 # Create your views here.
